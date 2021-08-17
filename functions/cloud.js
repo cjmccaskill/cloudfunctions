6de018dfcd36dd25cea20180661e747fb3a4316f
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-17 10:01:00 -0500

    fix request url

--- functions/cloud.js
+++ functions/cloud.js
@@ -18,7 +18,7 @@ exports.handler = async function (event, context) {
       httpMethod,
       headers,
       queryStringParameters,
-      body: JSON.parse(data),
+      body: data,
     }),
   };
 };
